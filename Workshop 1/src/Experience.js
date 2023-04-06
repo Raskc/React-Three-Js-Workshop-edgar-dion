fe88ed11f772a7035a310a2a2a0221b846e65d29
Raskc <afraieda@gmail.com>
2023-04-06 15:35:06 +0200

    [ADDED] simple la team

--- Workshop 1/src/Experience.js
+++ Workshop 1/src/Experience.js
@@ -1,5 +1,19 @@
-export default function Experience()
+import { useFrame } from '@react-three/fiber'
+import React, { useRef, useState } from 'react'
+
+export default function Experience(props)
 {
-    return <>
-    </>
+    const mesh = useRef()
+    const [active, setActive] = useState(false)
+    useFrame((state, delta) => (mesh.current.rotation.x += delta))
+    return (
+      <mesh
+        {...props}
+        ref={mesh}
+        scale={active ? 1.5 : 1}
+        onClick={(event) => setActive(!active)}>
+        <boxBufferGeometry></boxBufferGeometry>
+        <meshStandardMaterial></meshStandardMaterial>
+      </mesh>
+    )
 }
--- Workshop 1/src/index.js
+++ Workshop 1/src/index.js
@@ -2,17 +2,20 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.js'
-
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
         camera={ {
-            fov: 45,
+            fov: 110,
             near: 0.1,
-            far: 200,
+            far: 300,
             position: [ -4, 3, 6 ]
         } }
     >
-    </Canvas>
+    <ambientLight intensity={0.1} />
+    <directionalLight color="blue" position={[0, 0, 5]} />
+    <Experience position={[-1.2, 0, 0]} />
+    <Experience position={[1.2, 0, 0]} />
+    </Canvas>,
 )
